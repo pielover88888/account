e950791a737cb06edd5ae17118d67a51ed765ac9
Jeff <pielover88888@live.com>
2015-07-26 03:30:11 -0400

    proper session starting

--- form.php
+++ form.php
@@ -1,6 +1,13 @@
 <form method="POST" action="">
+<?php
+if($_POST["username"]){
+$username = $_POST["username"];
+} elseif ($_SESSION["login"]){
+$username = $_SESSION["login"];
+}
+?>
 <input type="hidden" name="username" value="<?= htmlspecialchars($_POST["username"])?>">
-<input type="hidden" name="authed" value="<?= htmlspecialchars($_POST["username"]) ?>">You're signed in as "<?= htmlspecialchars($_POST["username"]) ?>
+<input type="hidden" name="authed" value="<?= htmlspecialchars($username) ?>">You're signed in as "<?= htmlspecialchars($username) ?>"
 <br><input type="text" name="to" placeholder="to" value="<?= $_POST["to"]?>"> (case sensitive..)<br>
 <input type="text" name="title" placeholder="title"><br>
 <input type="text" name="message" placeholder="message">
