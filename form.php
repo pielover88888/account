1360d83c255807f92687f90acc494a436809df8a
Jeff <pielover88888@live.com>
2015-08-10 16:43:31 -0400

    Styles and tweaks

--- form.php
+++ form.php
@@ -1,3 +1,4 @@
+<div id="sendmes">
 <form method="POST" action="" id="sendmes">
 <?php
 if($_POST["username"]){
@@ -8,8 +9,9 @@ $username = $_SESSION["login"];
 ?>
 <input type="hidden" name="username" value="<?= htmlspecialchars($_POST["username"])?>">
 <input type="hidden" name="authed" value="<?= htmlspecialchars($username) ?>">You're signed in as "<?= htmlspecialchars($username) ?>"
-<br><input type="text" name="to" placeholder="to" value="<?= $_POST["to"]?>"> (case sensitive..)<br>
+<br><input type="text" name="to" placeholder="to ('name,name2' to send to many users)" value="<?= $_POST["to"]?>"> (case sensitive..)<br>
 <input type="text" name="title" placeholder="title"><br>
 <textarea type="text" name="message" placeholder="message"></textarea>
-<input type=submit>
-</form>
+<input type=submit class="button small">
+</form><div class="profile_edit"><a href="/account/?do=profile"><button class="button small">Edit Profile</button></a></div>
+</div>
